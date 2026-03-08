da4fe536d52ba17bb7bb35fde125d492454cfd67
Celestino Francisco <celestinofrancisco@Celestinos-MacBook-Air.local>
2026-03-07 23:51:25 -0600

    fix upload-url validation

--- index.js
+++ index.js
@@ -440,12 +440,6 @@ app.post("/upload-url", async (req, res) => {
     const uuidRegex =
       /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
-    if (!uuidRegex.test(userId)) {
-      return res.status(400).json({
-        ok: false,
-        error: "invalid userId format"
-      });
-    }
 
     if (!uuidRegex.test(jobId)) {
       return res.status(400).json({
